3b853027b777b8a5aa4cbaa2cba59e31306d255f
emildsn <emildsn00@gmail.com>
2023-07-06 17:00:47 -0300

    Initial Commit

---
+++ pendientes-app-ios/pendientes_app_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  pendientes_app_iosApp.swift
+//  pendientes-app-ios
+//
+//  Created by Emiliano Diaz on 06/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct pendientes_app_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
