89a31578e95558c2a209349bd3b42c29324c7254
emildsn <emildsn00@gmail.com>
2023-07-10 12:09:16 -0300

    add TextField and Button in NavigationView

--- pendientes-app-ios/ContentView.swift
+++ pendientes-app-ios/ContentView.swift
@@ -8,15 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var newTodo = ""
+    @State private var allTodos: [TodoItem] = []
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            VStack {
+                HStack {
+                    TextField("Agregar pendientes...", text: $newTodo)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    Button(action:  {
+                        guard !self.newTodo.isEmpty else { return }
+                        self.allTodos.append(TodoItem(todo: self.newTodo))
+                        self.newTodo = ""
+                    }) {
+                        Image(systemName: "plus")
+                    }
+                }.padding()
+                List {
+                    ForEach(allTodos) {todoItem in
+                        Text(todoItem.todo)
+                    }
+                }
+            }.navigationBarTitle("Pendientes", displayMode: .inline)
         }
-        .padding()
     }
+}
+
+
+struct TodoItem: Identifiable {
+    let id = UUID()
+    let todo: String
 }
 
 struct ContentView_Previews: PreviewProvider {
